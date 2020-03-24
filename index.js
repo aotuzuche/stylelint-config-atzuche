0d00bb4ce9eeaad19645b777349b96b8ddfd114b
Eason chiu <76670758@qq.com>
2020-03-24 15:03:39 +0800

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+module.exports = {
+  extends: 'stylelint-config-standard',
+  rules: {
+    'font-family-no-missing-generic-family-keyword': null,
+    'no-descending-specificity': null,
+    'at-rule-no-unknown': null,
+    'function-url-quotes': 'always',
+  },
+}
