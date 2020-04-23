43680542cbd18a37e7deda818c74e4a69ff1df69
Eason chiu <76670758@qq.com>
2020-04-23 23:35:21 +0800

    fix: 忽略tsx/jsx文件

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = {
   extends: 'stylelint-config-standard',
-  ignoreFiles: ['**/*.tsx', '**/*.jsx'],
   rules: {
     'font-family-no-missing-generic-family-keyword': null,
     'no-descending-specificity': null,
     'at-rule-no-unknown': null,
     'selector-type-no-unknown': null,
     'function-url-quotes': 'always',
+    'declaration-block-trailing-semicolon': null,
   },
 }
